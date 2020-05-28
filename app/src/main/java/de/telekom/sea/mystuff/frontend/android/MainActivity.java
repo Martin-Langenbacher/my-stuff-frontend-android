c9ffabf1f4c00d6425aaf011b93b3d2a54fa9af6
Martin Langenbacher <martin.langenbacher@telekom.de>
2020-05-28 16:21:17 +0200

    Übung mit David

--- app/src/main/java/de/telekom/sea/mystuff/frontend/android/MainActivity.java
+++ app/src/main/java/de/telekom/sea/mystuff/frontend/android/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -28,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         itemsList = findViewById(R.id.rv_items);
         //  --> check the dogsExample:  --> Mayo ==> refreshLayout = findViewById(R.id.refreshRideListLayout);
         viewModel = new ViewModelProvider(this).get(ItemListViewModel.class);                // warum macht man dies so kompliziert? --> ViewModelProvider: der Erzeuger der ViewModel, da die ViewModel kann länger leben als die MainActivity...
-        viewModel.initWithApplication(getApplication());  // springt in MyStuffViewModel, damit bekomme ich den Context --> Api-Factory sind definiert (Zentraler Bereich, wie ich an meine Dienste komme)Zeile macht:                                           // darum legt man die Daten ab und man holt sie die alte Instanz aus dem Speicher...
+        //viewModel.initWithApplication(getApplication());  // springt in MyStuffViewModel, damit bekomme ich den Context --> Api-Factory sind definiert (Zentraler Bereich, wie ich an meine Dienste komme)Zeile macht:                                           // darum legt man die Daten ab und man holt sie die alte Instanz aus dem Speicher...
         itemListAdapter = new ItemListRecyclerViewAdapter(new ArrayList<>());                       // Android kann sehr willkührlich sein.
         itemsList.setLayoutManager(new LinearLayoutManager(this));
         itemsList.setAdapter(itemListAdapter);  // verbindet Adapter mit RecyclerView; RecyclerView benutzt Adapter um die Zeilen aufzubauen.
@@ -43,7 +44,9 @@ public class MainActivity extends AppCompatActivity {
             if (listApiResponse.isSuccessful()){
                 itemListAdapter.updateList(listApiResponse.body);
             } else {
-                ((MyStuffApplication) getApplication()).getMyStuffContext().sendInfoMessage(listApiResponse.errorMessage);
+                Toast.makeText(MainActivity.this, listApiResponse.errorMessage, Toast.LENGTH_LONG).show();
+            // ohne myStuff... keine Fehlermeldung, also über Toast...
+               // ((MyStuffApplication) getApplication()).getMyStuffContext().sendInfoMessage(listApiResponse.errorMessage);
             }
         });
     }
--- app/src/main/java/de/telekom/sea/mystuff/frontend/android/ui/ItemListViewModel.java
+++ app/src/main/java/de/telekom/sea/mystuff/frontend/android/ui/ItemListViewModel.java
@@ -1,17 +1,27 @@
 package de.telekom.sea.mystuff.frontend.android.ui;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
 
 import java.util.List;
 
+import de.telekom.sea.mystuff.frontend.android.api.ApiFactory;
 import de.telekom.sea.mystuff.frontend.android.api.ApiResponse;
+import de.telekom.sea.mystuff.frontend.android.api.ItemApi;
 import de.telekom.sea.mystuff.frontend.android.model.Item;
-import de.telekom.sea.mystuff.frontend.android.util.MyStuffViewModel;
+import de.telekom.sea.mystuff.frontend.android.repo.ItemRepo;
 
-public class ItemListViewModel extends MyStuffViewModel {
+public class ItemListViewModel extends ViewModel {
 
+
+
+    // Ohne ...Context, ....
     public LiveData<ApiResponse<List<Item>>> getMyItems(){
-        return getMyStuffContext().getItemRepo().getAll();
+        ApiFactory apiFactory = new ApiFactory();
+        ItemApi itemApi = apiFactory.createApi(ItemApi.class); // Hier erzeuge ich das itemApi
+        //ItemRepo itemRepo = new ItemRepo(itemApi);
+        //return itemRepo.getAll();
+        return itemApi.getAll(); // Grund für Repo: Google-Empfehlung (wegen offline-Nutzung!)
     }
 
 }
